3ea5bf8a69cf8615a3b2574ecbe2806fffcaae11
Jeffrey Choi <jeffrey1choi@gmail.com>
2020-07-28 04:50:33 -0700

    added new functionlity

--- Assets/Coding Quiz.js
+++ Assets/Coding Quiz.js
@@ -1,6 +1,6 @@
 var questions = [
     {
-    prompt: "What is Javascript?(d) It is a free, open-source software using the permissive MIT License that's used in the majority of front-end developer work.",
+    currentQuestion: "What is Javascript?",
     answer: {
         a: "A scripting or programming language that allows you to implement complex features on web pages",
         b: "A cascading style sheet used to manipulate HTML code",
@@ -10,7 +10,7 @@ var questions = [
     correctAnswer:"a"
 } ,
 {
-    prompt: "What is const?",
+    currentQuestion: "What is const?",
     answer: {
         a: "A shorthand for 'continue' in video games",
         b: "Any variable that if declared can be updated or changed within the whole window",
@@ -20,7 +20,7 @@ var questions = [
     correctAnswer: "c"
 } ,
 {
-    prompt: "What is the correct html code to insert a line break?",
+    currentQuestion: "What is the correct html code to insert a line break?",
     answer: {
         a: "<break>",
         b: "<lb>",
@@ -30,7 +30,7 @@ var questions = [
     correctAnswer: "d"
 } ,
 {
-    prompt: "What is console.log?",
+    currentQuestion: "What is console.log?",
     answer: {
         a: "It logs the innerworkings of your code",
         b: "It logs the list of consoles you own SONY4LYFE",
@@ -40,7 +40,7 @@ var questions = [
     correctAnswer: "c"
 } ,
 {
-    prompt: "How do you create a function in JS?",
+    currentQuestion: "How do you create a function in JS?",
     answer: {
         a: "funtion: myFunction",
         b: "function myFunction",
@@ -50,29 +50,40 @@ var questions = [
     correctAnswer: "b"
 } ,
 ];
+
 var startBtn = document.getElementById("start-btn");
-startBtn.addEventListener("click", startGame);
-var answerChoices = document.getElementById("answer-choices")
+let insertQuestions = document.getElementById("questions");
+var answerChoices = document.getElementById("answer-choices");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
 var score = 0;
-var insertQuestions = document.getElementById("questions");
-var insertAnswers = document.getElementsByClassName("answer-btns");
+var curentScore = document.getElementById("currentScore")
 var showTime = document.getElementById("timer");
+var timeLeft = document.getElementById("seconds");
 var secondsLeft = 60;
-var yourResults = document.getElementsByClassName("results")
+var yourResults = document.getElementsByClassName("results");
+let lastQuestionIndex = (questions.length-1);
+let runningQuestionIndex = 0; 
+let shuffledQuestions, currentQuestionIndex;
+
+console.log(questions)
 
 function startGame() {
     startBtn.classList.add("hide");
     answerChoices.classList.remove("hide");
-    countdown();
-    for (var i =0; i < questions.length; i++) {
-        var currentQuestion = Math.floor(Math.random()*questions.length)
-        insertQuestions.innerText = questions[currentQuestion].prompt 
-    }
+    insertQuestions.classList.remove("hide");
+    countdown () ;
+    generateQuiz () ;
+    checkAnswer () ;
+    console.log(score)
+    console.log(secondsLeft)
     // for(var i =0; i<questions.length; i++) {
-    //     insertQuestions.innerText = questions[i].prompt
-    //     insertAnswers.innerText = questions[i].prompt
+    //     insertQuestions.innerText = questions[i].currentQuestion
+    //     insertAnswers.innerText = questions[i].currentQuestion
     //     // // event.stopPropagation()
-    //     let response = prompt(questions[i].prompt)
+    //     let response = currentQuestion(questions[i].currentQuestion)
     //     if (response == questions[i].answer){
     //         score ++
     //         alert("correct")
@@ -80,26 +91,59 @@ function startGame() {
     //         alert("wrong")
     //     }
     // }
-    console.log(countdown);
+}
+
+function generateQuiz (){
+    let q = questions[runningQuestionIndex];
+    insertQuestions.innerHTML = "<p>" + q.currentQuestion + "</p>";
+    choiceA.innerHTML = q.answer.a;
+    choiceB.innerHTML = q.answer.b;
+    choiceC.innerHTML = q.answer.c;
+    choiceD.innerHTML = q.answer.d;
+    console.log(q)
+}
+
+function checkAnswer (answer) {
+    if(answer == questions[runningQuestionIndex].correctAnswer) {
+        score ++
+        console.log(score)
+    } else if {
+        newSecondsLeft = (secondsLeft - 5) 
+        console.log(secondsLeft)
+    }
 }
 
 function countdown() {
-    var secondsInterval = setInterval(function() {
-        secondsLeft--;
-        showTime.textContent = secondsLeft + " seconds left";
-    
-        if(secondsLeft === 0) {
-          clearInterval(secondsInterval);
-          sendMessage();
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeLeft.innerHTML = secondsLeft + " seconds left till colorsplosion.";
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+    console.log(secondsLeft)
+  }
 
-        }
-      }, 1000);
+// function decreaseTime() {
+//     decreasedSecondsLeft = countdown() 
+// }
+function nextQuestion() {
 }
+
+// function countScore(answer) {
+//     if(questions[runningQuestionIndex].correctAnswer == answer) {
+//         score ++
+//     } else {
+//         secondsLeft - 5
+//     }
+// }
 
 function nextQuestion() {
 
 }
 
-function decreaseTime() {
-
-}
+// function decreaseTime() {
+//     secondsLeft - 5
+// }
+startBtn.addEventListener("click", startGame);
